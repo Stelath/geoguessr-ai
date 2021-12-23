dc01efaa79aee13ede00693c3fb90503ef61ec65
Alex <alex.k.korte@gmail.com>
2021-12-23 01:54:07 -0500

    Changed Model & Database Builder + Cleaned up Main.py

--- dataset_builder.py
+++ dataset_builder.py
@@ -1,8 +1,11 @@
 import scipy.io
 import argparse
 import csv
+import torchvision.transforms as transforms
+from torchvision.io import read_image
 from tqdm import tqdm
 from random import randint
+import numpy as np
 import os
 
 def get_args():
@@ -11,39 +14,92 @@ def get_args():
     parser.add_argument("--output", help="The output folder", required=True, type=str)
     return parser.parse_args()
 
+# def main():
+#     args = get_args()
+    
+#     train_files = []
+#     val_files = []
+    
+#     mat = scipy.io.loadmat('GPS_Long_Lat_Compass.mat')
+#     coords = mat['GPS_Long_Lat_Compass']
+    
+#     for coord_index in tqdm(range(len(coords))):
+#         lat, lon = coords[coord_index][0], coords[coord_index][1]
+        
+#         # Set to training or testing data
+#         if -76 <= lon <= 70: 
+#             if randint(0, 9) == 0:
+#                 for i in range(6):
+#                     val_files.append([f'images/{str(coord_index + 1).zfill(6)}_{i}.jpg', [lat, lon]])
+#             else:
+#                 for i in range(5):
+#                     train_files.append([f'images/{str(coord_index + 1).zfill(6)}_{i}.jpg', [lat, lon]])
+        
+#     with open(f'{args.output}/train_data.csv', mode='w', encoding='utf8') as f:
+#         writer = csv.writer(f)
+#         writer.writerows(train_files)
+    
+#     with open(f'{args.output}/val_data.csv', mode='w', encoding='utf8') as f:
+#         writer = csv.writer(f)
+#         writer.writerows(train_files)
+        
+#     print('Train Files:', len(train_files))
+#     print('Val Files:', len(val_files))
+#     print('Total Files:', len(train_files) + len(val_files))
+#     print('Train Files to Val Files Ratio:', len(train_files) / len(val_files))
+
+args = get_args()
+    
+train_data = []
+val_data = []
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                    std=[0.229, 0.224, 0.225])
+transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        normalize,
+    ])
+
+def get_data(coord, coord_index):
+    lat, lon = coord[0], coord[1]
+    img_arr = []
+    for i in range(6):
+        img = read_image(f'images/{str(coord_index + 1).zfill(6)}_{i}.jpg')
+        img = transform(img)
+        img_arr.append(img)
+        
+    return [img_arr, [lat, lon]]
+
 def main():
-    args = get_args()
-    
-    train_files = []
-    val_files = []
-    
     mat = scipy.io.loadmat('GPS_Long_Lat_Compass.mat')
     coords = mat['GPS_Long_Lat_Compass']
     
-    for coord_index in tqdm(range(len(coords))):
-        lat, lon = coords[coord_index][0], coords[coord_index][1]
-        
+    val_count = 0
+    train_count = 0
+    
+    for coord_index, coord in tqdm(enumerate(coords)):
+        lon = coord[0]
         # Set to training or testing data
         if -76 <= lon <= 70: 
             if randint(0, 9) == 0:
-                for i in range(6):
-                    val_files.append([f'images/{str(coord_index + 1).zfill(6)}_{i}.jpg', [lat, lon]])
+                data = get_data(coord, coord_index)
+                val_data.append(data)
+                val_count += 1
             else:
-                for i in range(5):
-                    train_files.append([f'images/{str(coord_index + 1).zfill(6)}_{i}.jpg', [lat, lon]])
-        
-    with open(f'{args.output}/train_data.csv', mode='w', encoding='utf8') as f:
-        writer = csv.writer(f)
-        writer.writerows(train_files)
+                data = get_data(coord, coord_index)
+                train_data.append(data)
+                train_count += 1
     
-    with open(f'{args.output}/val_data.csv', mode='w', encoding='utf8') as f:
-        writer = csv.writer(f)
-        writer.writerows(train_files)
-        
-    print('Train Files:', len(train_files))
-    print('Val Files:', len(val_files))
-    print('Total Files:', len(train_files) + len(val_files))
-    print('Train Files to Val Files Ratio:', len(train_files) / len(val_files))
+    np.random.shuffle(train_data)
+    np.save('train_data.npy', self.training_data)
+    
+    np.random.shuffle(val_data)
+    np.save('val_data.npy', self.training_data)
+    
+    print('Train Files:', train_count)
+    print('Val Files:', val_count)
 
 if __name__ == '__main__':
     main()
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import time
 import warnings
 from enum import Enum
 
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.parallel
@@ -15,7 +16,6 @@ import torch.optim
 import torch.multiprocessing as mp
 import torch.utils.data
 import torch.utils.data.distributed
-import torchvision.transforms as transforms
 import custom_datasets as datasets
 from model import resnext101
 
@@ -28,11 +28,12 @@ parser.add_argument('--epochs', default=90, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
+parser.add_argument('--checkpoint-step', default=1, type=int, metavar='N',
+                    help='how often (in epochs) to save the model (default: 1)')
 parser.add_argument('-b', '--batch-size', default=256, type=int,
                     metavar='N',
                     help='mini-batch size (default: 256), this is the total '
-                         'batch size of all GPUs on the current node when '
-                         'using Data Parallel or Distributed Data Parallel')
+                         'batch size of the GPU')
 parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
                     metavar='LR', help='initial learning rate', dest='lr')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
@@ -46,23 +47,10 @@ parser.add_argument('--resume', default='', type=str, metavar='PATH',
                     help='path to latest checkpoint (default: none)')
 parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
                     help='evaluate model on validation set')
-parser.add_argument('--world-size', default=-1, type=int,
-                    help='number of nodes for distributed training')
-parser.add_argument('--rank', default=-1, type=int,
-                    help='node rank for distributed training')
-parser.add_argument('--dist-url', default='tcp://224.66.41.62:23456', type=str,
-                    help='url used to set up distributed training')
-parser.add_argument('--dist-backend', default='nccl', type=str,
-                    help='distributed backend')
 parser.add_argument('--seed', default=None, type=int,
                     help='seed for initializing training. ')
 parser.add_argument('--gpu', default=None, type=int,
                     help='GPU id to use.')
-parser.add_argument('--multiprocessing-distributed', action='store_true',
-                    help='Use multi-processing distributed training to launch '
-                         'N processes per node, which has N GPUs. This is the '
-                         'fastest way to use PyTorch for either single node or '
-                         'multi node data parallel training')
 
 best_acc1 = 0
 
@@ -83,75 +71,27 @@ def main():
     if args.gpu is not None:
         warnings.warn('You have chosen a specific GPU. This will completely '
                       'disable data parallelism.')
-
-    if args.dist_url == "env://" and args.world_size == -1:
-        args.world_size = int(os.environ["WORLD_SIZE"])
-
-    args.distributed = args.world_size > 1 or args.multiprocessing_distributed
-
-    ngpus_per_node = torch.cuda.device_count()
-    if args.multiprocessing_distributed:
-        # Since we have ngpus_per_node processes per node, the total world_size
-        # needs to be adjusted accordingly
-        args.world_size = ngpus_per_node * args.world_size
-        # Use torch.multiprocessing.spawn to launch distributed processes: the
-        # main_worker process function
-        mp.spawn(main_worker, nprocs=ngpus_per_node, args=(ngpus_per_node, args))
-    else:
-        # Simply call main_worker function
-        main_worker(args.gpu, ngpus_per_node, args)
+    
+    main_worker(args.gpu, args)
 
 
-def main_worker(gpu, ngpus_per_node, args):
+def main_worker(gpu, args):
     global best_acc1
     args.gpu = gpu
 
     if args.gpu is not None:
         print("Use GPU: {} for training".format(args.gpu))
-
-    if args.distributed:
-        if args.dist_url == "env://" and args.rank == -1:
-            args.rank = int(os.environ["RANK"])
-        if args.multiprocessing_distributed:
-            # For multiprocessing distributed training, rank needs to be the
-            # global rank among all the processes
-            args.rank = args.rank * ngpus_per_node + gpu
-        dist.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
-                                world_size=args.world_size, rank=args.rank)
     
-    print("=> creating model '{}'".format(args.arch))
+    print("=> creating model: ResNeXt 3D - 101 layers")
     model = resnext101(num_classes=2)
 
     if not torch.cuda.is_available():
         print('CPU, this will be slow')
-    elif args.distributed:
-        # For multiprocessing distributed, DistributedDataParallel constructor
-        # should always set the single device scope, otherwise,
-        # DistributedDataParallel will use all available devices.
-        if args.gpu is not None:
-            torch.cuda.set_device(args.gpu)
-            model.cuda(args.gpu)
-            # When using a single GPU per process and per
-            # DistributedDataParallel, we need to divide the batch size
-            # ourselves based on the total number of GPUs we have
-            args.batch_size = int(args.batch_size / ngpus_per_node)
-            args.workers = int((args.workers + ngpus_per_node - 1) / ngpus_per_node)
-            model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
-        else:
-            model.cuda()
-            # DistributedDataParallel will divide and allocate batch_size to all
-            # available GPUs if device_ids are not set
-            model = torch.nn.parallel.DistributedDataParallel(model)
     elif args.gpu is not None:
         torch.cuda.set_device(args.gpu)
         model = model.cuda(args.gpu)
     else:
-        # DataParallel will divide and allocate batch_size to all available GPUs
-        if args.arch.startswith('alexnet') or args.arch.startswith('vgg'):
-            model.features = torch.nn.DataParallel(model.features)
-            model.cuda()
-        else:
-            model = torch.nn.DataParallel(model).cuda()
+        model = torch.nn.DataParallel(model).cuda()
 
     # define loss function (criterion) and optimizer
     criterion = nn.L1Loss().cuda(args.gpu)
@@ -187,34 +127,21 @@ def main_worker(gpu, ngpus_per_node, args):
     # Data loading code
     train_data = os.path.join(args.data, 'train_data.csv')
     val_data = os.path.join(args.data, 'val_data.csv')
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
-
-    train_dataset = datasets.CustomImageDataset(
-        train_data,
-        transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            normalize,
-        ]))
 
     if args.distributed:
         train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
     else:
         train_sampler = None
-
+        
+    train_data_path = os.path.join(args.data, 'training_data.npy')
+    train_dataset = np.load(train_data_path, allow_pickle=True)
     train_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),
         num_workers=args.workers, pin_memory=True, sampler=train_sampler)
-
-    val_loader = torch.utils.data.DataLoader(
-        datasets.CustomImageDataset(val_data, transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            normalize,
-        ])),
+    
+    val_data_path = os.path.join(args.data, 'val_data.npy')
+    val_dataset = np.load(Val_data_path, allow_pickle=True)
+    val_loader = torch.utils.data.DataLoader(val_dataset,
         batch_size=args.batch_size, shuffle=False,
         num_workers=args.workers, pin_memory=True)
 
@@ -236,12 +163,10 @@ def main_worker(gpu, ngpus_per_node, args):
         # remember best acc@1 and save checkpoint
         is_best = acc1 > best_acc1
         best_acc1 = max(acc1, best_acc1)
-
-        if not args.multiprocessing_distributed or (args.multiprocessing_distributed
-                and args.rank % ngpus_per_node == 0):
+        
+        if epoch % args.checkpoint_step == 0:
             save_checkpoint({
                 'epoch': epoch + 1,
-                'arch': args.arch,
                 'state_dict': model.state_dict(),
                 'best_acc1': best_acc1,
                 'optimizer' : optimizer.state_dict(),
